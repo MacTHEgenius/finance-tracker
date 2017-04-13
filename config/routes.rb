43f69be84de2cccb5400da9bfa0e0c754cd912df
Benoit Levesque <genius.blevesque@gmail.com>
2017-04-13 14:20:39 +0000

    Update views and navigations

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users, controllers: { registrations: "user/registrations" }
   root to: 'welcome#index'
   
   get 'portfolio', to: "users#portfolio"
